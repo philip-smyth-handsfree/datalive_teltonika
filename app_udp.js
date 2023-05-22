05610400109c9247033aa83b3a5e17821be2cca7
Phil Smyth <philip.smyth@handsfree.co.uk>
2023-05-22 09:15:35 +0100

    Updated udp listener

--- app_udp.js
+++ app_udp.js
@@ -1,14 +1,29 @@
-import dgram from 'node:dgram';
+var dgram = require('dgram');
 
 const udpPort = 5000
 const server = dgram.createSocket('udp4');
 
 server.on('error', (err) => {
-  logger.error(`UDP server error:\n${err.stack}`);
+  console.log(`UDP server error:\n${err.stack}`);
   server.close();
 });
 
 server.on('message', async (msg, rinfo) => {
   const today = new Date();
-  logger.info(`** ---- UDP server a new packet: ${msg} (msg length: ${msg.length}) from ${rinfo.address}:${rinfo.port}, at: ${today}`);
+  console.log(`** ---- UDP server a new packet: ${msg} (msg length: ${msg.length}) from ${rinfo.address}:${rinfo.port}, at: ${today}`);
+
+  try {
+    // With a UDP Packet, we need to send back
+    // Length - Packet ID - Not usable Byte 0x01 - AVL Packet ID - Number of accepted Data
+  } catch (error) {
+    console.log(`** UDP ERROR **::\n${error}`);
+  }
 });
+
+server.on('listening', () => {
+  const address = server.address();
+  // console.log(`UDP server listening ${address.address}:${address.port}`);
+  console.log(`UDP server listening ${address.address}:${address.port}`);
+});
+
+server.bind(udpPort);
